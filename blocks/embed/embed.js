54c4dc0b2ca0df66941aacdb7f96286174e92355
Benjamin Christiani <benjamin.christiani@emea.merkleinc.com>
2024-02-19 17:22:17 +0100

    Touched embed script

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -9,7 +9,7 @@ const loadScript = (url, callback, type) => {
   const script = document.createElement('script');
   script.src = url;
   if (type) {
-      script.setAttribute('type', type);
+    script.setAttribute('type', type);
   }
   script.onload = callback;
   head.append(script);
@@ -57,32 +57,32 @@ const embedTwitter = (url) => {
 
 const loadEmbed = (block, link, autoplay) => {
   if (block.classList.contains('embed-is-loaded')) {
-      return;
+    return;
   }
 
   const EMBEDS_CONFIG = [
-      {
-          match: ['youtube', 'youtu.be'],
-          embed: embedYoutube,
-      },
-      {
-          match: ['vimeo'],
-          embed: embedVimeo,
-      },
-      {
-          match: ['twitter'],
-          embed: embedTwitter,
-      },
+    {
+      match: ['youtube', 'youtu.be'],
+      embed: embedYoutube,
+    },
+    {
+      match: ['vimeo'],
+      embed: embedVimeo,
+    },
+    {
+      match: ['twitter'],
+      embed: embedTwitter,
+    },
   ];
 
   const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => link.includes(match)));
   const url = new URL(link);
   if (config) {
-      block.innerHTML = config.embed(url, autoplay);
-      block.classList = `block embed embed-${config.match[0]}`;
+    block.innerHTML = config.embed(url, autoplay);
+    block.classList = `block embed embed-${config.match[0]}`;
   } else {
-      block.innerHTML = getDefaultEmbed(url);
-      block.classList = 'block embed';
+    block.innerHTML = getDefaultEmbed(url);
+    block.classList = 'block embed';
   }
   block.classList.add('embed-is-loaded');
 };
@@ -93,21 +93,21 @@ export default function decorate(block) {
   block.textContent = '';
 
   if (placeholder) {
-      const wrapper = document.createElement('div');
-      wrapper.className = 'embed-placeholder';
-      wrapper.innerHTML = '<div class="embed-placeholder-play"><button type="button" title="Play"></button></div>';
-      wrapper.prepend(placeholder);
-      wrapper.addEventListener('click', () => {
-          loadEmbed(block, link, true);
-      });
-      block.append(wrapper);
+    const wrapper = document.createElement('div');
+    wrapper.className = 'embed-placeholder';
+    wrapper.innerHTML = '<div class="embed-placeholder-play"><button type="button" title="Play"></button></div>';
+    wrapper.prepend(placeholder);
+    wrapper.addEventListener('click', () => {
+      loadEmbed(block, link, true);
+    });
+    block.append(wrapper);
   } else {
       const observer = new IntersectionObserver((entries) => {
-          if (entries.some((e) => e.isIntersecting)) {
-              observer.disconnect();
-              loadEmbed(block, link);
-          }
-      });
-      observer.observe(block);
+        if (entries.some((e) => e.isIntersecting)) {
+          observer.disconnect();
+          loadEmbed(block, link);
+        }
+    });
+    observer.observe(block);
   }
 }
